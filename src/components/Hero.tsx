ac3d4b01eca6d6e83b62740eb8aea17f00308424
Sakith-Seneviratne <Sakithsene@gmail.com>
2025-07-04 11:41:05 +0530

    style: update gradient background and border color in Hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,7 @@
 export const Hero = () => {
   return (
     <div className="bg-gradient-to-b from-black via-[#120d42] to-[#004091] text-white py-[72px] relative overflow-clip">
-      <div className="absolute h-[375px] w-[750px] lg:w-[2400px] lg:h-[1200px] rounded-[100%] bg-black left-1/2 -translate-x-1/2 border-[#3F8BEC] bg-[radial-gradient(closest-side,#000_82%,#3F8BEC)] top-[calc(100%-96px)]"></div>
+      <div className="absolute h-[375px] w-[750px] lg:w-[2400px] lg:h-[1200px] rounded-[100%] bg-black left-1/2 -translate-x-1/2 border-[#004091] bg-[radial-gradient(closest-side,#000_82%,#3F8BEC)] top-[calc(100%-96px)]"></div>
       <div className="container relative pt-24">
         <div className="flex flex-col items-center justify-center text-center">
           <h1 className="text-7xl sm:text-9xl tracking-tighter font-bold mt-8">
