0078bc86aa5348bc30d5e40829539f50df7bf953
Sakith-Seneviratne <Sakithsene@gmail.com>
2025-07-24 10:03:20 +0530

    feat: update AboutFeatureCard styles for improved layout; add Tezzeract logo to AboutFeatures section

--- src/components/AboutFeatureCard.tsx
+++ src/components/AboutFeatureCard.tsx
@@ -5,7 +5,7 @@ export default function AboutFeatureCard(props: {title: string, description: str
 {
     const { title, description, children } = props;
     return (
-        <div className="bg-neutral-800 border border-white/10 rounded-xl container mx-auto flex flex-col mt-12 gap-8">
+        <div className="bg-neutral-800 border p-6 border-white/10 rounded-xl container mx-auto flex flex-col mt-12 gap-8">
             <div>{children}</div>
             <div>
                 <h3 className="text-3xl text-white font-medium mt-6">{title}</h3>
--- src/components/AboutFeatures.tsx
+++ src/components/AboutFeatures.tsx
@@ -1,11 +1,18 @@
 import AboutFeatureCard from "./AboutFeatureCard";
+import TezImage from "../assets/images/tezzeractofficial_logo.jpeg";
 
 export default function AbouutFeatures() {
     return(
-    <section className="container">
-        <div>
+    <section className="container mx-auto">
+        <div className="mt-12 px-5 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
    <AboutFeatureCard title="Industris" description="sfownfjsdngfdsjongsdjf"></AboutFeatureCard>
+   <div>
+    <div className="">
+<img src={TezImage} alt="Tezzeract Logo" />
+    </div>
+   </div>
     <AboutFeatureCard title="dsfdsfsfs" description="sfownfjsdng23r32rfdsjongsdjf"></AboutFeatureCard>
+   <AboutFeatureCard title="dsfqw3r32r3" description="23rfsafafaadf"></AboutFeatureCard>
    <AboutFeatureCard title="dsfqw3r32r3" description="23rfsafafaadf"></AboutFeatureCard>
 
             <div></div>
