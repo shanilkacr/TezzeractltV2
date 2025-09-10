65fd06ba29ef47dae5b25b88ac5ec131228a9c11
Sakith-Seneviratne <Sakithsene@gmail.com>
2025-09-10 16:33:25 +0530

    pages

--- src/app/bookcall/page.tsx
+++ src/app/bookcall/page.tsx
@@ -3,13 +3,13 @@
 
 import React from "react";
 import Image from "next/image";
-import BgImage from "@/assets/images/Tezzeract-Background.webp"; // Only works if using webpack with image imports
+import BgImage from "@/assets/images/BgTez.png"; // Only works if using webpack with image imports
 
 function BookCall() {
   return (
-    <div className="flex w-full h-screen">
+    <div className="flex w-full h-full">
       {/* Left Side */}
-      <div className="w-1/2 h-full relative">
+      <div className="w-1/2 h-cover relative">
         <Image
           src={BgImage}
           alt="Background"
@@ -20,14 +20,14 @@ function BookCall() {
       </div>
 
       {/* Right Side */}
-      <div className="w-1/2 h-full flex flex-col p-[100px] justify-center bg-white">
-        <h2 className="tezzeracth2 text-4xl md:text-5xl font-display font-light text-black mb-4 !leading-tight">
+      <div className="w-1/2 h-[150%] flex flex-col p-[100px] justify-center bg-white">
+        <h2 className=" text-4xl md:text-5xl font-medium text-[#9AE1FE] mb-4 ">
           Take the first step to scale your company
         </h2>
 
         <iframe
-          src="https://meet.brevo.com/thefind-hotel/borderless?l=30-minute-meeting"
-          className="w-full h-[600px] border-0"
+          src="https://meet.brevo.com/shanilka-rajapaksha-1"
+          className="w-full h-[1250px] border-0"
           title="Brevo Booking Form"
         ></iframe>
       </div>
